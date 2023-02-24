f550186467ac277a225433c14fa5c0fb1a30cd89
Rodrigo <rodrigo.marquesg@gmail.com>
2023-02-23 22:19:42 -0300

    Configuracao dos Middlewares express-session e connect-flash

--- app.js
+++ app.js
@@ -6,8 +6,24 @@
     const admin = require("./routes/admin")
     const path = require('path')
     const mongoose = require('mongoose');
+    const session = require('express-session');
+    const flash = require('connect-flash');
 
 //Configurações
+    //Session
+        app.use(session({
+            secret: "123456",
+            ressave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash())
+
+    //MIDLDLEWARE
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash("success_msg");
+            res.locals.error_msg = req.flash("error_msg");
+            next();
+        })
     //Body Parser
         app.use(bodyParser.urlencoded({extended:true}));
         app.use(bodyParser.json());
@@ -25,6 +41,11 @@
     //Public
         app.use(express.static(path.join(__dirname,"public")))
 
+        app.use((req, res, next) => {
+            console.log("MIDDLEWARE!!")
+            next();
+        })
+
 //Rotas
     app.get('/', (req,res) => {
         res.send('Main Page');
@@ -34,7 +55,7 @@
         res.send("Posts List");
     })
     app.use('/admin', admin);
-    
+
 
 //Outros
 const PORT = 8081;
